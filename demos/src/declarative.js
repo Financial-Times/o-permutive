b587d6044fce2281291e4c11ef603acaa595ef00
alevito <alessandro.vito84@gmail.com>
2019-05-23 15:52:26 +0100

    Separate demos declarative and programmatic init

---
+++ demos/src/declarative.js
@@ -0,0 +1,16 @@
+/*global require*/
+import oPermutive from '../../main.js';
+
+// document.addEventListener('DOMContentLoaded', function() {
+// 	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
+// });
+const oPermConf = {
+	"appInfo": {
+		"appName": "article",
+		"contentId": "5cfae92e-6cc5-11e9-80c7-60ee53e6681d"
+	},
+};
+
+document.cookie = 'FTConsent=behaviouraladsOnsite%3Aon;';
+
+//oPermutive.pAddon({}, oPermConf);
