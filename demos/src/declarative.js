9d8a39226e8d5624fb1f344bd7c3312bfd1739fb
alevito <alessandro.vito84@gmail.com>
2019-05-23 15:57:36 +0100

    in demo: pass userDemog and pageData to pAddon

--- demos/src/declarative.js
+++ demos/src/declarative.js
@@ -1,10 +1,11 @@
 /*global require*/
 import oPermutive from '../../main.js';
 
-// document.addEventListener('DOMContentLoaded', function() {
-// 	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
-// });
-const oPermConf = {
+const userDemog = {
+	"some": "thing"
+}
+
+const pageData = {
 	"appInfo": {
 		"appName": "article",
 		"contentId": "5cfae92e-6cc5-11e9-80c7-60ee53e6681d"
@@ -13,4 +14,4 @@ const oPermConf = {
 
 document.cookie = 'FTConsent=behaviouraladsOnsite%3Aon;';
 
-//oPermutive.pAddon({}, oPermConf);
+oPermutive.pAddon(userDemog, pageData);
