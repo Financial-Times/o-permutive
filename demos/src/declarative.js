e444d9435951108e59dba7d7ce53436d50d60490
alevito <alessandro.vito84@gmail.com>
2019-05-24 15:36:56 +0100

    Updated declarative demo page metadata schema

--- demos/src/declarative.js
+++ demos/src/declarative.js
@@ -1,18 +1,24 @@
 /*global require*/
-import oPermutive from '../../main.js';
-
-const userDemog = {
-	"some": "thing"
-};
-
-const pageData = {
-	"appInfo": {
-		"appName": "article",
-		"contentId": "5cfae92e-6cc5-11e9-80c7-60ee53e6681d"
-	},
-};
+import Permutive from '../../main.js';
 
 document.cookie = 'FTConsent=behaviouraladsOnsite%3Aon;';
 
 // Set the page metadata
-oPermutive.pAddon(userDemog, pageData);
+Permutive.pAddon({
+	type: "article", // e.g. "home" or "article"
+	article: {
+		id: "5cfae92e-6cc5-11e9-80c7-60ee53e6681d",
+		title: "A nice article indeed!",
+		type: "economics", // genre
+		people: ["John Dorie", "Karl Marx", "Adam Smith"],
+		categories: ["blueberries", "savings", "money"],
+		authors: ["John Unknown"],
+		topics: ["vegetables", "drama", "flowers"],
+		admants: ["another", "list", "of", "string"]
+	},
+	user: {
+		industry: "Oil and gas",
+		position: "Salesman",
+		responsibility: "I manage pipelines"
+	}
+});
