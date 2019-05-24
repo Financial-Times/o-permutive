80a7124f1e4a0c03e2bdde4a2d07a46efb3ed148
alevito <alessandro.vito84@gmail.com>
2019-05-24 15:35:22 +0100

    Updated programmatic demo with right schema

--- demos/src/programmatic.js
+++ demos/src/programmatic.js
@@ -2,17 +2,17 @@
 import Permutive from '../../main.js';
 
 let oPermConf = {
-	"appInfo": {
-		"appName": "article",
-		"contentId": "5cfae92e-6cc5-11e9-80c7-60ee53e6681d"
+	appInfo: {
+		appName: "article",
+		contentId: "5cfae92e-6cc5-11e9-80c7-60ee53e6681d",
 	},
-	"publicApiKeys": {
-		"id": "e1c3fd73-dd41-4abd-b80b-4278d52bf7aa",
-		"key": "b2b3b748-e1f6-4bd5-b2f2-26debc8075a3"
+	publicApiKeys: {
+		id: "e1c3fd73-dd41-4abd-b80b-4278d52bf7aa",
+		key: "b2b3b748-e1f6-4bd5-b2f2-26debc8075a3",
 	},
-	"adsApi": {
-		"user": "https://ads-api.ft.com/v1/user",
-		"content": "https://ads-api.ft.com/v1/content/"
+	adsApi: {
+		user: "https://ads-api.ft.com/v1/user",
+		content: "https://ads-api.ft.com/v1/content/",
 	}
 };
 
@@ -23,7 +23,20 @@ new Permutive(false, oPermConf);
 
 // Set the page metadata
 Permutive.pAddon({
-	user: "something",
-}, {
-	contentId: "123",
+	type: "article", // e.g. "home" or "article"
+	article: {
+		id: "5cfae92e-6cc5-11e9-80c7-60ee53e6681d",
+		title: "A nice article indeed!",
+		type: "economics", // genre
+		people: ["John Dorie", "Karl Marx", "Adam Smith"],
+		categories: ["blueberries", "savings", "money"],
+		authors: ["John Unknown"],
+		topics: ["vegetables", "drama", "flowers"],
+		admants: ["another", "list", "of", "string"]
+	},
+	user: {
+		industry: "Oil and gas",
+		position: "Salesman",
+		responsibility: "I manage pipelines"
+	}
 });
